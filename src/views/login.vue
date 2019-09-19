eb77c8c4f1a9e06b036a050c2084a300a22d2e09
unknown <pelumioni25@gmail.com>
2019-09-19 12:42:12 +0100

    first github commit

---
+++ src/views/login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6 px-0 d-none d-md-block">
+        <div class="login"></div>
+      </div>
+      <div class="col-lg-6 px-0">
+        <div class="login-container">
+          <div class="row">
+            <div class="col-lg-8 m-auto">
+              <div class="row">
+                <div class="col-12">
+                  <h4 class="b-34">Welcome back</h4>
+                  <p class="p-16">Enter your details to get back on track</p>
+                </div>
+                <div class="col-5">
+                  <button class="btn btn-login w-100">
+                    <img src="../../public/img/google.png" height="20px" alt /> Login with Google
+                  </button>
+                </div>
+                <div class="col-5">
+                  <button class="btn btn-login w-100">
+                    <img src="../../public/img/linkedin.svg" height="20px" alt /> Login with LinkedIn
+                  </button>
+                </div>
+                <div class="col-lg-10 mt-5">
+                  <div class="login-form mb-4">
+                    <label for>Email Address</label>
+                    <input type="email" />
+                  </div>
+                  <div class="login-form">
+                    <label for>Password</label>
+                    <input type="password" />
+                  </div>
+                </div>
+                <div class="col-12 mb-5">
+                  <a href="#" class="b-15">I forgot my password</a>
+                </div>
+                <div class="col-lg-4">
+                  <button class="btn btn-orange w-100">LOGIN</button>
+                </div>
+                <div class="col-lg-8 pt-2">
+                  <p class="b-15">
+                    You don’t have an Account ?
+                    <router-link
+                      to="/register"
+                      class="b-15"
+                      style="color: #00a6a6; "
+                    >Register</router-link>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+h4.b-34 {
+  margin-left: -6px;
+}
+</style>
---
+++ src/views/register.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6 px-0 d-none d-md-block">
+        <div class="login"></div>
+      </div>
+      <div class="col-lg-6 px-0">
+        <div class="login-container">
+          <div class="row">
+            <div class="col-lg-8 m-auto">
+              <div class="row">
+                <div class="col-12">
+                  <h4 class="b-34">Nice to have you here</h4>
+                  <p class="p-16">Just a few steps to get you started</p>
+                </div>
+                <div class="col-6">
+                  <button class="btn btn-login w-100">
+                    <img src="../../public/img/google.png" height="20px" alt /> Sign up with Google
+                  </button>
+                </div>
+                <div class="col-6">
+                  <button class="btn btn-login w-100">
+                    <img src="../../public/img/linkedin.svg" height="20px" alt /> Sign up with LinkedIn
+                  </button>
+                </div>
+                <div class="col-lg-12 mt-3">
+                  <div class="login-form mb-2">
+                    <label for>Firstname</label>
+                    <input type="text" />
+                  </div>
+                  <div class="login-form mb-2">
+                    <label for>Lastname</label>
+                    <input type="text" />
+                  </div>
+                  <div class="login-form mb-2">
+                    <label for>Email Address</label>
+                    <input type="email" />
+                  </div>
+                </div>
+                <div class="col-lg-6"></div>
+                <div class="col-lg-6"></div>
+                <div class="col-lg-12">
+                  <div class="login-form">
+                    <label for>Password</label>
+                    <input type="password" />
+                  </div>
+                </div>
+                <div class="col-12 mb-5">
+                  <a href="#" class="b-15">I forgot my password</a>
+                </div>
+                <div class="col-lg-4">
+                  <button class="btn btn-orange w-100">LOGIN</button>
+                </div>
+                <div class="col-lg-8 pt-2">
+                  <p class="b-15">
+                    You don’t have an Account ?
+                    <a
+                      href="#"
+                      class="b-15"
+                      style="color: #00a6a6; "
+                    >Register</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+h4.b-34 {
+  margin-left: -6px;
+}
+</style>
